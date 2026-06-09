83cccb60f70bd01a9062ccba56d0aa592a4a6e2f
srv005 <agrawalsaurav546@gmail.com>
2026-06-09 23:59:27 +0530

    Added redirect after login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require("passport");
+const{saveRedirectUrl} = require("../middleware.js");    
+
+
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
@@ -35,14 +38,17 @@ router.get("/login",(req,res)=>{
 
 router.post(
     "/login",
+    saveRedirectUrl,
     passport.authenticate("local",{
     failureRedirect:'/login',
     failureFlash:true,
 }),
 async(req,res)=>{
     req.flash("success","Welcome back!");
-    res.redirect("/listings");
-});
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+}
+);
 
 router.get("/logout",(req,res)=>{
     req.logout((err)=>{
