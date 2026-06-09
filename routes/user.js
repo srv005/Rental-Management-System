813fac49ff540fc13c5bb0df851d2fee9a5b35d9
srv005 <agrawalsaurav546@gmail.com>
2026-06-09 21:51:45 +0530

    Added login and user authentication

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const User = require("../models/user.js");
 const wrapAsync = require('../utils/wrapAsync');
-
+const passport = require("passport");
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
@@ -17,9 +17,24 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     res.redirect("/listings");
     } catch(e){
         req.flash("error",e.message);
-        res.redirect("/signup");
+        res.redirect("/login");
     }
 })
 );
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local",{
+    failureRedirect:'/login',
+    failureFlash:true,
+}),
+async(req,res)=>{
+    req.flash("success","Welcome back!");
+    res.redirect("/listings");
+});
 
 module.exports = router;
