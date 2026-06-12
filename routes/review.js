1f4ee30fb640d3976d0daaccef906d539827151a
srv005 <agrawalsaurav546@gmail.com>
2026-06-12 15:25:23 +0530

    completed authorization for reviews and listings

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utils/wrapAsync.js")
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview, isLoggedIn } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 //Post Review route
 router.post("/",
@@ -12,7 +12,7 @@ router.post("/",
     wrapAsync(async(req,res)=>{
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
@@ -22,7 +22,10 @@ router.post("/",
 
 }));
 //Delete Review route
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(async(req,res)=>{
     let {id,reviewId} = req.params;
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
